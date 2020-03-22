631c84c10d2a9962791a478f9147a2a3d24133df
Tyson3206792 <54131201+Tyson3206792@users.noreply.github.com>
2020-03-22 18:59:41 +1100

    Update camera.js

--- camera.js
+++ camera.js
@@ -1,8 +1,43 @@
+//Variables
+var w, h, wid, hei = 200;
+var y = 100;
+var mod = 'test'
+var model;		//Stores CNN
+
+var timer;
+var seconds;
+var mobile = mobilecheck();
+
+var video = document.querySelector("#videoElement");	//Webcam
+var canvas = document.getElementById('mycanvas');		//Displayed image
+var context = canvas.getContext('2d');
+var canvas2 = document.getElementById("mycanvas2");		//Predictions graph
+var context2 = canvas2.getContext("2d");
+var canvas3 = document.getElementById("mycanvas3");		//Cropped image for processing
+var context3 = canvas3.getContext("2d");
+var storedOut = document.getElementById('storedInput');
+var output = document.getElementById('predictedInput');
+var facing = "user";
+var then;
+var vals = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','_','Nothing'];
+var rand_char = -1;
+var score = 0;
+var positions;
+const videoSelect = document.querySelector('select#videoSource');
+const selectors = [videoSelect];
+videoSelect.onchange = beginStream;
+navigator.mediaDevices.enumerateDevices().then(gotDevices).catch(handleError);
+video.style.display="none";
+canvas3.style.display="none";
+if(!mobile)
+    videoSelect.style.display="none";
+
+//Functions
 function mode(func){
   var buttons = ['test', 'translate', 'learn'];
   mod = func;
   storedOut.textContent = '';
-  output.textContent = '';
+  output.textContent = 'Loading...';
   for(i = 0; i < 3; i++){
     var current = document.getElementById(buttons[i]);
     if(buttons[i] == func){
@@ -62,7 +97,8 @@ function update_canvas(){
     	if(facing == "user")
         	context.scale(-1, 1);
     	context.fillText(rand_char, canvas.width-150, 80);
-    }
+	}
+	return 1;
 }
 
 async function collectImage(){      //Retrives image from within prediction square
@@ -90,6 +126,7 @@ async function collectImage(){      //Retrives image from within prediction squa
 }
 
 async function runDetection() {
+    drawn = await update_canvas();
     imgData = await collectImage();
     done = await predict(imgData);
     console.log("Predicted");
@@ -169,7 +206,7 @@ function display(val){
 
 function countDown(){
 	clearInterval(timer);
-	seconds = 60;
+	seconds = 30;
 	timer = setInterval(function() {
 		output.textContent = seconds;
 		if (seconds <= 0){
@@ -190,78 +227,13 @@ function countDown(){
 function mobilecheck() {
   var check = false;
   (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-  console.log(check);
+  console.log("Is mobile device: " + check);
   return check;
 };
 
-//START CODE
-var w, h, wid, hei = 200;
-var y = 100;
-var mod = 'test'
-var model;		//Stores CNN
-//load_model();	//Load CNN
-
-var timer;
-var seconds;
-var mobile = mobilecheck();
-
-var cur = document.getElementById(mod);
-cur.style.backgroundColor = 'rgb(0, 102, 153)';
-cur.style.color = 'rgb(222, 222, 222)';
-
-//Stores the processed image. Remove in html page when not testing
-var video = document.querySelector("#videoElement");		//Webcam
-var canvas = document.getElementById('mycanvas');		//Stores cropped image
-var context = canvas.getContext('2d');
-var canvas2 = document.getElementById("mycanvas2");		//predictions
-var context2 = canvas2.getContext("2d");
-var canvas3 = document.getElementById("mycanvas3");		//Stores further processed image
-var context3 = canvas3.getContext("2d");
-var storedOut = document.getElementById('storedInput');
-var output = document.getElementById('predictedInput');
-var facing= "environment";
-var then;
-var vals = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','_','Nothing'];
-var rand_char = -1;
-var score = 0;
-var positions;
-video.style.display="none";
-canvas3.style.display="none";
-
-//Used to access webcam
-function access_webcam(){
-    if(facing == "user" && mobile){
-        facing = "environment";
-        constraints = {
-            "video": {
-                "facingMode":
-                { "ideal": facing }
-            }
-        };
-    }
-    else{
-        facing = "user";
-        constraints = {
-            "video": {
-                "facingMode":
-                { "ideal": facing }
-            }
-        };
-    }
-	if (navigator.mediaDevices.getUserMedia) {				//Access webcam
-	  navigator.mediaDevices.getUserMedia(constraints)
-		.then(function (stream) {
-		  video.srcObject = stream;
-		  ready = true;
-		})
-		.catch(function (err0r) {
-		  console.log("Something went wrong!");
-		});
-	}
-}
-
 async function load_model(){
-	access_webcam();
+    mode(mod);
+	beginStream();
 	console.log('CNN loading...')
     model = await tf.loadLayersModel('https://raw.githubusercontent.com/Tyson3206792/ASLCNN/master/trained_models/a-zmulti_0/model.json');
     console.log('CNN loaded.')
@@ -269,9 +241,68 @@ async function load_model(){
     output.textContent = 'Translating...';
     w = model.inputs[0].shape[1];
     h = model.inputs[0].shape[2];
-    update_canvas();
 	runDetection();
-	setInterval(update_canvas, 50);
 }
 
+//Begins the program loop
 load_model();
+
+//Webcam access code below taken from WebRTC github page and modified to suit the project.
+//https://github.com/webrtc/samples/blob/gh-pages/src/content/devices/input-output/js/main.js
+
+/*
+*  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
+*
+*  Use of this source code is governed by a BSD-style license
+*  that can be found in the LICENSE file in the root of the source
+*  tree.
+*/
+
+function gotDevices(deviceInfos) {
+  // Handles being called several times to update labels. Preserve values.
+  const values = selectors.map(select => select.value);
+  while (videoSelect.firstChild) {
+    videoSelect.removeChild(videoSelect.firstChild);
+  }
+  for (let i = 0; i !== deviceInfos.length; ++i) {
+    const deviceInfo = deviceInfos[i];
+    const option = document.createElement('option');
+    option.value = deviceInfo.deviceId;
+	if (deviceInfo.kind === 'videoinput') {
+      option.text = deviceInfo.label || `camera ${videoSelect.length + 1}`;
+      videoSelect.appendChild(option);
+    } else {
+      console.log('Some other kind of source/device: ', deviceInfo);
+    }
+  }
+  selectors.forEach((select, selectorIndex) => {
+    if (Array.prototype.slice.call(select.childNodes).some(n => n.value === values[selectorIndex])) {
+      select.value = values[selectorIndex];
+    }
+  });
+  facing = videoSelect.options[videoSelect.selectedIndex].innerHTML.includes("back") ? "environment" : "user";
+}
+
+function gotStream(stream) {
+  window.stream = stream; // make stream available to console
+  video.srcObject = stream;
+  // Refresh button list in case labels have become available
+  return navigator.mediaDevices.enumerateDevices();
+}
+
+function handleError(error) {
+  console.log('navigator.MediaDevices.getUserMedia error: ', error.message, error.name);
+}
+
+function beginStream() {
+  if (window.stream) {
+    window.stream.getTracks().forEach(track => {
+      track.stop();
+    });
+  }
+  const videoSource = videoSelect.value;
+  const constraints = {
+    video: {deviceId: videoSource ? {exact: videoSource} : undefined}
+  };
+  navigator.mediaDevices.getUserMedia(constraints).then(gotStream).then(gotDevices).catch(handleError);
+}
