3add91691a1c1d3651380245278948abcda10a74
Tyson3206792 <54131201+Tyson3206792@users.noreply.github.com>
2020-05-30 17:46:54 +1000

    Add files via upload

--- image_capture.js
+++ image_capture.js
@@ -5,21 +5,7 @@ const context = canvas.getContext("2d");
 var canvas2 = document.getElementById("mycanvas2");    
 var context2 = canvas2.getContext("2d");
 
-var canvas3 = document.getElementById("mycanvas3");  
-var context3 = canvas3.getContext("2d");
-canvas3.width = 60;
-canvas3.height = 480;
-
-let isVideo = false;
-let model = null;
-console.log(1);
-const modelParams = {
-    flipHorizontal: false,   // flip e.g for video  
-    maxNumBoxes: 1,        // maximum number of boxes to detect
-    iouThreshold: 0.5,      // ioU threshold for non-max suppression
-    scoreThreshold: 0.6,    // confidence threshold for predictions.
-}
-count = max_pics = 400;
+count = max_pics = 50;
 key = '';
 
 document.addEventListener('keypress', dload);
@@ -30,49 +16,25 @@ function dload(e) {
   count = 0;
 }
 
-function startVideo() {
-    handTrack.startVideo(video).then(function (status) {
-        console.log("video started", status);
-        if (status) {
-            isVideo = true
-            runDetection()
-        }
-    });
-}
+function runDetection() {	
+	canvas.width = 640;
+	canvas.height = 480;
+	context.scale(-1, 1);
+	context.drawImage(video, 0, 0, -640, 480); 
+	context2.drawImage(video, -80, -150);      
+	//Take image in bounding box
+	//imgData = context.getImageData(x, y, wid, hei);   //Edited image
+	//Place image on new canvas
+	//context2.putImageData(imgData, 0, 0); 
+	context.beginPath();
+	context.strokeStyle = "#0000FF";
+	context.strokeRect(-x, y, -wid, hei);    
 
-function runDetection() {
-    model.detect(video).then(predictions => {
-        if(predictions.length == 1){
-            console.log("Predictions: ", predictions[0].bbox);
-            console.log("Predictions: ", predictions[0].bbox[0]);
-            wid = Math.ceil(predictions[0].bbox[2]);
-            hei = Math.ceil(predictions[0].bbox[3]);
-            x = Math.ceil(predictions[0].bbox[0]);  
-            y = Math.ceil(predictions[0].bbox[1]);  
-
-            //Resize canvas to fit prediction image
-            canvas2.width = wid;
-            canvas2.height = hei;
-
-            context.drawImage(video, 0, 0);      
-
-            //Take image in bounding box
-            imgData = context.getImageData(x, y, wid, hei);   //Edited image
-            //Place image on new canvas
-            context2.putImageData(imgData, 0, 0);
-
-            if(count < max_pics){
-                save_images();          
-            }
-        }
-        
-
-        model.renderPredictions(predictions, canvas, context, video);
-
-        if (isVideo) {
-            requestAnimationFrame(runDetection);
-        }
-    });
+	if(count < max_pics){
+		save_images();          
+	}
+	console.log("loop");
+	setTimeout(runDetection, 200);
 }
 
 function save_images(){
@@ -86,13 +48,26 @@ function save_images(){
     count++;
 }
 
-// Load the model.
-handTrack.load(modelParams).then(lmodel => {
-    // detect objects in the image.
-    console.log(2);
+function access_webcam(){
+	if (navigator.mediaDevices.getUserMedia) {				//Access webcam
+	  navigator.mediaDevices.getUserMedia({ video: true })
+		.then(function (stream) {
+		  video.srcObject = stream;
+		  ready = true;
+		})
+		.catch(function (err0r) {
+		  console.log("Something went wrong!");
+		});
+	}
+}
 
-    model = lmodel;
-    console.log(3);
-    startVideo();
-    console.log(4);
-});
+var x = 360;
+var y = 150;
+var wid = 200;
+var hei = 200;
+
+access_webcam();
+
+//setInterval(runDetection, 100);
+setTimeout(runDetection, 100);
+
